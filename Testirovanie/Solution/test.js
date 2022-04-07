1ed1c7b2876f3b6c2d0cd920212801fbeed1ac26
NeAnastasia <81082567+NeAnastasia@users.noreply.github.com>
2022-04-07 21:17:04 +0700

    fixes

--- Testirovanie/Solution/test.js
+++ Testirovanie/Solution/test.js
@@ -21,6 +21,48 @@ describe("Tests", () => {
       assert.equal(compare.comp(a1, a2), true);
     });
     
+    it("Comp_\"Same\"ArraysFraction_True", () => { //new дробные
+      let a1 = [11.11, 19.1, 13.3, 5.5];
+      let a2 = [11.11*11.11, 19.100*19.10, 13.3*13.3, 5.5*5.5];
+      assert.equal(compare.comp(a1, a2), true);
+    });
+
+    it("Comp_\"Same\"ArraysNegative_True", () => { //new отрицательные
+      let a1 = [-11, -19, -13, -5];
+      let a2 = [-11*-11, -19*-19, -13*-13, -5*-5];
+      assert.equal(compare.comp(a1, a2), true);
+    });
+
+    it("Comp_\"Same\"ArraysMaxValues_True", () => { //new number values
+      let a1 = [Number.MAX_VALUE, Number.MIN_VALUE];
+      let a2 = [Number.MAX_VALUE * Number.MAX_VALUE, Number.MIN_VALUE * Number.MIN_VALUE];
+      assert.equal(compare.comp(a1, a2), true);
+    });
+
+    it("Comp_ArraysDifferentNegative_False", () => { //new отрицательные 
+      let a1 = [-11, 19, 13, -5];
+      let a2 = [-11*11, 19*19, 13*13, -5*5];
+      assert.equal(compare.comp(a1, a2), false);
+    });
+
+    it("Comp_ArraysDifferentSize(secondlower)_False", () => { //new разный размер 
+      let a1 = [11, 19, 13, 5, 6];
+      let a2 = [11*11, 19*19, 13*13, 5*5];
+      assert.equal(compare.comp(a1, a2), false);
+    });
+
+    it("Comp_ArraysDifferentSize(firstlower)_False", () => { //new разный размер 
+      let a1 = [11, 19, 13];
+      let a2 = [11*11, 19*19, 13*13, 5*5];
+      assert.equal(compare.comp(a1, a2), false);
+    });
+
+    it("Comp_ArraysDifferentSizeBecauseOfDuplicate_False", () => { //new разный размер из-за дублирования
+      let a1 = [11, 19, 13];
+      let a2 = [11*11, 19*19, 13*13, 13*13];
+      assert.equal(compare.comp(a1, a2), false);
+    });
+
     it("Comp_ActuallySameArrays_False", () => {
       a1 = [11, 19, 13, 5];
       a2 = [11, 19, 13, 5];
